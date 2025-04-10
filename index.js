2e2c553adc6804fe676828d2835719ef4eebdf52
dlwoals-123 <dlwoals574@naver.com>
2025-04-10 16:22:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-    user: 'process.env.DB_USER',
-    host: 'process.env.DB_HOST',
-    database: 'process.env.DB_NAME',
-    password: 'process.env.DB_PASSWORD,',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT || 5432,
     ssl: {
         rejectUnauthorized: false
